8756ac3b13789d4bda7a82221fef7b5229dbacf3
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2022-08-02 09:13:42 +0900

    [fix]

--- api/server.js
+++ api/server.js
@@ -7,15 +7,15 @@ app.use(express.static('js'));
 app.use(express.static('images'));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/../index.html`);
+  res.sendFile(`./index.html`);
 });
 
 app.get('/test', (req, res) => {
-  res.sendFile(`${__dirname}/../home.html`);
+  res.sendFile(`./home.html`);
 });
 
 app.use((req, res, next) => {
-  res.sendFile(`${__dirname}/error/404.html`);
+  res.sendFile(`./error/404.html`);
 });
 let server = app.listen(3000, function(){
   console.log("Node.js is listening to PORT:" + server.address().port);
